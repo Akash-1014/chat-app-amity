1ae33b77e60581a0c4cfa090627d7b2322d5717b
sahoo-commits <chandan.sahoo@antiersolutions.com>
2023-07-28 17:11:22 +0530

    fdsaflksaf

---
+++ src/Components/Redux/RecentChat/action.js
@@ -0,0 +1,114 @@
+export const RECENT_LOADING = "RECENT_LOADING";
+export const RECENT_ERROR = "RECENT_ERROR";
+export const ADD_RECENT_CHAT = "ADD_RECENT_CHAT";
+export const NEW_CREATED_CHAT = "NEW_CREATED_CHAT";
+import { selectChat } from "../Chatting/action";
+import { ChannelRepository, ChannelType } from "@amityco/js-sdk";
+export const recentLoding = (payload) => ({ type: RECENT_LOADING, payload });
+export const recentError = (payload) => ({ type: RECENT_ERROR, payload });
+
+export const recentChatResult = (payload) => ({
+  type: ADD_RECENT_CHAT,
+  payload,
+});
+export const newCreatedChat = (payload) => ({
+  type: NEW_CREATED_CHAT,
+  payload,
+});
+
+export const makeRecentChatApi = (token) => async (dispatch) => {
+  recentLoding(true);
+  // const url = `https://messenger-clo.herokuapp.com/chat`;
+  try {
+    //   let res = await fetch(url, {
+    //     method: "get",
+    //     headers: {
+    //       "content-type": "application/json",
+    //       Authorization: `Bearer ${token}`,
+    //     },
+    //   });
+    //   let data = await res.json();
+    dispatch(recentChatResult(data));
+  } catch (err) {
+    dispatch(recentError(true));
+    console.log(err.message);
+  }
+};
+
+export const makeNewGroup = (group_data, token) => async (dispatch) => {
+  // recentLoding(true);
+  const url = `https://messenger-clo.herokuapp.com/chat/group`;
+  console.log("group_data", group_data);
+  try {
+    // let res = await fetch(url, {
+    //   method: "post",
+    //   body: JSON.stringify(group_data),
+    //   headers: {
+    //     "content-type": "application/json",
+    //     Authorization: `Bearer ${token}`,
+    //   },
+    // });
+    // let data = await res.json();
+    // console.log("data: ", data);
+    const liveChannel = ChannelRepository.createChannel({
+      channelId: group_data.name,
+      type: "community",
+      userIds: group_data.users,
+    });
+
+    liveChannel.once("dataUpdated", (model) => {
+      console.log(`Channel created: ${model.channelId}`);
+    });
+
+    liveChannel.once("dataError", (error) => {
+      // Handle channel create error (non-unique channelID)
+    });
+    dispatch(newCreatedChat(group_data));
+  } catch (err) {
+    dispatch(recentError(true));
+    console.log(err.message);
+  }
+};
+
+export const accessChat = (userId, token, recentchat) => async (dispatch) => {
+  dispatch(recentLoding(true));
+  const url = `https://messenger-clo.herokuapp.com/chat`;
+  try {
+    // let res = await fetch(url, {
+    //   method: "post",
+    //   body: JSON.stringify({ userId }),
+    //   headers: {
+    //     "content-type": "application/json",
+    //     Authorization: `Bearer ${token}`,
+    //   },
+    // });
+    // let data = await res.json();
+    // console.log(data);
+    if (!recentchat.find((el) => el._id === data._id)) {
+      dispatch(newCreatedChat(data));
+      dispatch(
+        selectChat({
+          isGroupChat: data.isGroupChat,
+          index: 0,
+          user: data.users.find((el) => el._id == userId),
+          _id: data._id,
+          chatName: data.chatName,
+        })
+      );
+      return;
+    }
+    dispatch(recentLoding(false));
+    dispatch(
+      selectChat({
+        isGroupChat: data.isGroupChat,
+        index: 0,
+        user: data.users.find((el) => el._id == userId),
+        _id: data._id,
+        chatName: data.chatName,
+      })
+    );
+  } catch (err) {
+    dispatch(recentError(true));
+    console.log(err.message);
+  }
+};
